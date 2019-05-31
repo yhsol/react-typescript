16e0a572d32e3000aaeefbbdd518e5f47ef782ac
yhsol <yhsol1592@gmail.com>
2019-05-31 17:34:22 +0900

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import { Form, Input } from "./input";
 const App: React.FC = () => {
   const [counter, setCounter] = useState<number>(0);
   const [value, setValue] = useState<string>("");
+
   const onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
     console.log(event.target);
   };
+
   const onFormSubmit = (event: React.FormEvent) => {
     event.preventDefault();
   };
+
   const add = () => {
     setCounter(counter + 1);
   };
+
   return (
     <>
       <Form onFormSubmit={onFormSubmit}>
